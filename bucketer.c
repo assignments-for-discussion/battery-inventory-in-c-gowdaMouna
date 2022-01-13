1118c41a433839de8e8716c5e9efecc8361e3126
gowdaMouna <79515404+gowdaMouna@users.noreply.github.com>
2022-01-13 18:45:41 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
-#include <assert.h>
 
-struct CountsByUsage {
-  int lowCount;
-  int mediumCount;
-  int highCount;
-};
-
-struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
-  struct CountsByUsage counts = {0, 0, 0};
-  return counts;
-}
-
-void testBucketingByNumberOfCycles() {
-  const int chargeCycleCounts[] = {100, 300, 500, 600, 900, 1000};
-  const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
-  printf("Counting batteries by usage cycles...\n");
-  struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
-  assert(counts.lowCount == 2);
-  assert(counts.mediumCount == 3);
-  assert(counts.highCount == 1);
-  printf("Done counting :)\n");
-}
 
 int main() {
-  testBucketingByNumberOfCycles();
+   int charged_cycles;
+  scanf("%d",&charged_cycles)
+    if(charged_cycles<400)
+      printf("low");
+  elseif(charged_cycles<=919)
+  printf("medium");
+  else
+    printf("High");    
   return 0;
 }
